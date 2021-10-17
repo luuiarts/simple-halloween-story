e1ba1313a92fa155e47b5a30d0e51e1fc9939203
Noah <noahm1216@gmail.com>
2021-10-16 19:02:10 -0700

    Mobile inputs working!
    - no real things to do other than Gyro and tapping to spawn cubes

--- UnityProject_MobileStarted/Assets/Scripts/MobileInputReads.cs
+++ UnityProject_MobileStarted/Assets/Scripts/MobileInputReads.cs
@@ -11,6 +11,9 @@ using UnityEngine;
 /// </summary>
 public class MobileInputReads : MonoBehaviour
 {
+
+    private Vector2 initialPosition;
+
     public GameObject particle;
 
 
@@ -26,17 +29,43 @@ public class MobileInputReads : MonoBehaviour
         //loop the touch points
         foreach (Touch touch in Input.touches)
         {
+            print("touch");
+
+            // if the touch phase has started
             if (touch.phase == TouchPhase.Began)
             {
+                print("touch tap / hold");
+
+
+                initialPosition = touch.position;
+
                 // Construct a ray from the current touch coordinates
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 if (Physics.Raycast(ray))
                 {
                     // Create a particle if hit
                     Instantiate(particle, transform.position, transform.rotation);
-                }
-            }
+                }//end of ray
+
+            }//end of touch phase beginning (tap)
+            //else we swiped
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                print("touch swiped");
+
+                // get the moved direction compared to the initial touch position
+                var direction = touch.position - initialPosition;
+
+                // get the signed x direction
+                // if(direction.x >= 0) 1 else -1
+                var signedDirection = Mathf.Sign(direction.x);
+
+                print("direction swipe: " + direction + " ... signDir: " + signedDirection);
+               
+            }//end of swiped
+
         }//end of loop touches
+      
 
     }//end of update
 
